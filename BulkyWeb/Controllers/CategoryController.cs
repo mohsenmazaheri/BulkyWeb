3459910f39aa31ffad96c18d574c2132406c0409
mohsenmazaheri <113507860+mohsenmazaheri@users.noreply.github.com>
2025-11-13 20:00:36 +0330

    Server side validations are added for category controller and view

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -25,9 +25,24 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            //Customized validation for checking equal values
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name!");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value!");
+            }
+
+            // Standard validation of the properties of the model 
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -10,9 +10,12 @@ namespace BulkyWeb.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required]
-        [DisplayName("Display Name")]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        //[Range(1,100)] // Deafult error message
+        [Range(1, 100, ErrorMessage="Display Order must be between 1-100")] // Customized error message
         public int DisplayOrder { get; set; }
     }
 }
