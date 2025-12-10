76fc93e540a06409142685507d0b0f00d7333c1e
mohsenmazaheri <113507860+mohsenmazaheri@users.noreply.github.com>
2025-12-10 19:20:34 +0330

    Admin and Customer views are added to the project

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -4,8 +4,9 @@ using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Bulky.DataAccess.Controllers
+namespace BulkyWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
