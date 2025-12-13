8d4599bd0f6edf100cc98c44d874247d06b21cb0
mohsenmazaheri <113507860+mohsenmazaheri@users.noreply.github.com>
2025-12-13 16:36:09 +0330

    Create and Edit Product Veiws and Controller are combined into Upsert. TinyMCE editor is added

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -23,19 +23,12 @@ namespace BulkyWeb.Areas.Admin.Controllers
             return View(productList);
         }
 
-        public IActionResult Create()
+        /// <summary>
+        /// Update or Insert
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Upsert(int? id) 
         {
-            //-------- Projection in EF -------
-            // Dropdown list items are not in the model. So, we make them manually and use it by a ViewBag
-            //IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll()
-            //    .Select(a => new SelectListItem
-            //    {
-            //        Text = a.Name,
-            //        Value = a.Id.ToString()
-            //    });
-
-            //ViewBag.CategoryList = CategoryList;
-            //ViewData["CategoryList"] = CategoryList;
 
             ProductVM productVM = new()
             {
@@ -45,19 +38,25 @@ namespace BulkyWeb.Areas.Admin.Controllers
                     Text = a.Name,
                     Value = a.Id.ToString()
                 })
-                ,Product = new Product()
+                ,
+                Product = new Product()
             };
             //---------------------------------
-            //return View();
-            return View(productVM);
+            // Create
+            if (id == null || id == 0)
+            {
+                return View(productVM);
+            }
+            else // Update
+            {
+                productVM.Product = _unitOfWork.Product.Get(a => a.Id == id);
+                return View(productVM);
+            }
         }
 
         [HttpPost]
-        //public IActionResult Create(Product obj)
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
-            
-            // Standard validation of the properties of the model 
             if (ModelState.IsValid)
             {
                 _unitOfWork.Product.Add(productVM.Product);
@@ -77,34 +76,6 @@ namespace BulkyWeb.Areas.Admin.Controllers
             }
         }
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            var product = _unitOfWork.Product.Get(a => a.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return View(product);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product updated successfully";
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
-
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
@@ -113,8 +84,6 @@ namespace BulkyWeb.Areas.Admin.Controllers
             }
 
             var product = _unitOfWork.Product.Get(a => a.Id == id);
-            //var category2 = _db.Categories.FirstOrDefault(c => c.Id == id);
-            //Product? category3 = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
             if (product == null)
             {
                 return NotFound();
