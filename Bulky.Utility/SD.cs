cc2c72d263182df3cbfd6aa0d70c54e18792a335
mohsenmazaheri <113507860+mohsenmazaheri@users.noreply.github.com>
2026-03-30 19:34:30 +0330

    Order processing buttons logic is implemented

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -18,7 +18,7 @@ namespace Bulky.Utility
         public const string StatusInProcess = "Processing";
         public const string StatusShipped = "Shipped";
         public const string StatusCancelled = "Cancelled";
-        public const string StatusRefund = "Refund";
+        public const string StatusRefunded = "Refunded";
 
         public const string PaymentStatusPending = "Pending";
         public const string PaymentStatusApproved = "Approved";
--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -13,6 +13,7 @@ using System.Security.Claims;
 namespace BulkyWeb.Areas.Admin.Controllers
 {
     [Area("admin")]
+    [Authorize]
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -51,7 +52,7 @@ namespace BulkyWeb.Areas.Admin.Controllers
             orderHeaderFromDb.City = OrderVM.OrderHeader.City;
             orderHeaderFromDb.State = OrderVM.OrderHeader.State;
             orderHeaderFromDb.PostalCode = OrderVM.OrderHeader.PostalCode;
-            if(!string.IsNullOrEmpty(OrderVM.OrderHeader.Carrier))
+            if (!string.IsNullOrEmpty(OrderVM.OrderHeader.Carrier))
                 orderHeaderFromDb.Carrier = OrderVM.OrderHeader.Carrier;
             if (!string.IsNullOrEmpty(OrderVM.OrderHeader.TrackingNumber))
                 orderHeaderFromDb.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
@@ -63,43 +64,79 @@ namespace BulkyWeb.Areas.Admin.Controllers
             return RedirectToAction(nameof(Details), new { orderId = orderHeaderFromDb.Id });
         }
 
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult StartProcessing()
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(OrderVM.OrderHeader.Id, SD.StatusInProcess);
+            _unitOfWork.Save();
+            TempData["Success"] = "Order Details Updated Successfully.";
+            return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
+        }
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult ShipOrder()
+        {
+            var orderHeader = _unitOfWork.OrderHeader.Get(a => a.Id == OrderVM.OrderHeader.Id);
+            orderHeader.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+            orderHeader.Carrier = OrderVM.OrderHeader.Carrier;
+            orderHeader.OrderStatus = SD.StatusShipped;
+            orderHeader.ShippingDate = DateTime.Now;
+            if(orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+            {
+                orderHeader.PaymentDueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(30));
+            }
+            
+            _unitOfWork.OrderHeader.Update(orderHeader);
+            _unitOfWork.Save();
+            TempData["Success"] = "Order Shipped Successfully.";
+            return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
+        }
+
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll(string status)
         {
-            IEnumerable<OrderHeader> orderHeaders;
+            IEnumerable<OrderHeader> orderHeaders = null;
 
             if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
                 orderHeaders = _unitOfWork.OrderHeader.GetAll(null, includeProperties: "ApplicationUser").ToList();
             else
             {
-                var claimsIdentity = (ClaimsIdentity)User.Identity;
-                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
-                orderHeaders = _unitOfWork.OrderHeader.GetAll(a => a.ApplicationUserId == userId, includeProperties: "ApplicationUser");
+                ClaimsIdentity? claimsIdentity = (ClaimsIdentity)User.Identity;
+                if (claimsIdentity != null)
+                {
+                    string userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+                    orderHeaders = _unitOfWork.OrderHeader.GetAll(a => a.ApplicationUserId == userId, includeProperties: "ApplicationUser");
+                }
             }
-                    
-            switch (status)
+
+            if (orderHeaders != null)
             {
-                case "pending":
-                    orderHeaders = orderHeaders.Where(o => o.PaymentStatus == SD.PaymentStatusDelayedPayment);
-                    //orderHeaders = orderHeaders.Where(o => o.PaymentStatus == SD.StatusPending);
-                    break;
-                case "inprocess":
-                    orderHeaders = orderHeaders.Where(o => o.PaymentStatus == SD.StatusInProcess);
-                    break;
-                case "completed":
-                    orderHeaders = orderHeaders.Where(o => o.PaymentStatus == SD.StatusShipped);
-                    break;
-                case "approved":
-                    orderHeaders = orderHeaders.Where(o => o.PaymentStatus == SD.StatusApproved);
-                    break;
-                default:
-                    break;
+                switch (status)
+                {
+                    case "pending":
+                        orderHeaders = orderHeaders.Where(o => o.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                        //orderHeaders = orderHeaders.Where(o => o.PaymentStatus == SD.StatusPending);
+                        break;
+                    case "inprocess":
+                        orderHeaders = orderHeaders.Where(o => o.PaymentStatus == SD.StatusInProcess);
+                        break;
+                    case "completed":
+                        orderHeaders = orderHeaders.Where(o => o.PaymentStatus == SD.StatusShipped);
+                        break;
+                    case "approved":
+                        orderHeaders = orderHeaders.Where(o => o.PaymentStatus == SD.StatusApproved);
+                        break;
+                    default:
+                        break;
+                }
             }
 
             return Json(new { data = orderHeaders });
         }
-        
+
         #endregion
     }
 }
